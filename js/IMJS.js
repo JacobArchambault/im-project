68d69a60438501bee879462109b85822244ae3fb
JacobArchambault <39110529+JacobArchambault@users.noreply.github.com>
2018-12-28 11:13:53 -0500

    Add files via upload
    Improved accessibility

--- js/IMJS.js
+++ js/IMJS.js
@@ -1,26 +1,24 @@
 // 1 Variables
-    // 1.1 Defined Functions
-    let appear = function (x) {
-    linksObjects[x].section.fadeToggle();
-    
-    };
-        
-    // 1.2 Defined Arrays
+    // 1.1 Defined Arrays
     let linksObjects = [
-    {link:$(".about-link"), section:$("#about")},
-    {link:$(".classes-link"), section:$("#classes")},
-    {link:$(".calendar-link"), section:$("#calendar")},
-    {link:$(".contact-link"), section:$("#contact")},
-    ];
+        {link:$(".about-link"), section:$("#about")},
+        {link:$(".classes-link"), section:$("#classes")},
+        {link:$(".calendar-link"), section:$("#calendar")},
+        {link:$(".contact-link"), section:$("#contact")},
+        ];
 
 // 2 JQuery functions on startup
 $("#foreground-image").hide().fadeIn(2500);
 $(".text-section").hide();
-
+$("#about").fadeIn();
+    
 // 3 Functions for interacting with the page
 for (var i=0; i<linksObjects.length; i++) {
     let y = i;
     linksObjects[i].link.on("click",  function () {
-        appear(y);
+        $("section").hide();
+        linksObjects[y].section.fadeIn();
     })
-    }  // with the Appear function defined above, this function makes text appear/disappear when the link to it is clicked on
+}  // This function makes text appear when the link to it is clicked on
+
+// Note to self: check to see if ".text-section" class can be safely replaced with "section" element selector
