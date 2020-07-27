e49f41183d5e9e4cd2f49c4282c4754e05a05581
Jacob Archambault <jacob.archambault@waystar.com>
2020-07-27 01:46:40 -0400

    Add grow and shrink functionality to picture on hover, and clean up code a bit

--- js/IMJS.js
+++ js/IMJS.js
@@ -1,24 +1,46 @@
-// 1 Variables
-    // 1.1 Defined Arrays
-    let linksObjects = [
-        {link:$(".about-link"), section:$("#about")},
-        {link:$(".classes-link"), section:$("#classes")},
-        {link:$(".calendar-link"), section:$("#calendar")},
-        {link:$(".contact-link"), section:$("#contact")},
-        ];
+// 1 Function declarations
 
-// 2 JQuery functions on startup
+// Adds 25px to the width and height of the foreground image element.
+function growImage(){
+  $("#foreground-image").animate({
+    height: '+=25px',
+    width: '+=25px'
+  });
+}
+
+// Subtracts 25px from the width and height of the foreground image element.
+function shrinkImage(){
+  $("#foreground-image").animate({
+    height: '-=25px',
+    width: '-=25px'
+  });
+} 
+
+// 2 Variables
+    // 2.1 Defined Arrays
+let linksObjects = [
+	{link:$(".about-link"), section:$("#about")},
+	{link:$(".classes-link"), section:$("#classes")},
+	{link:$(".calendar-link"), section:$("#calendar")},
+	{link:$(".contact-link"), section:$("#contact")},
+	];
+
+// 3 function calls
+// 3.1 JQuery functions on startup
 $("#foreground-image").hide().fadeIn(2500);
 $(".text-section").hide();
 $("#about").fadeIn();
-    
-// 3 Functions for interacting with the page
+
+// 3.2 jQuery function call to grow and shrink image on hovering 
+$("#foreground-image").hover(growImage, shrinkImage); 
+
+// This function makes text appear when the link to it is clicked on.
 for (var i=0; i<linksObjects.length; i++) {
     let y = i;
     linksObjects[i].link.on("click",  function () {
+		// hide all sections.
         $("section").hide();
-        linksObjects[y].section.fadeIn();
+		// fade in the section associated with the link.
+        linksObjects[y].section.fadeIn(1000);
     })
-}  // This function makes text appear when the link to it is clicked on
-
-// Note to self: check to see if ".text-section" class can be safely replaced with "section" element selector
+}
